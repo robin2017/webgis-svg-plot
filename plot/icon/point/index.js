bbb34251d7766c92d78b80fd3c90e1cb6838c9fc
robin2017 <robin.seu@foxmail.com>
2019-09-22 21:39:27 +0800

    优化lib

--- plot/icon/point/index.js
+++ plot/icon/point/index.js
@@ -2,10 +2,18 @@
  * 自动生成ret.svg,只对其中cx,cy,x,y,x1,x2,y1,y2进行变化
  * 对points,d没有做变化，需要手动操作
  * */
+const config = {
+    fromFile:'point-lib-origin.svg',
+    toFile:'ret44.svg',
+    dx:-56,
+    dy:-32
+}
+
+
 const fs = require('fs');
 const regEx = /\s[x,y][1,2]="([0-9\.]*?)"|\sc[x,y]="([0-9\.]*?)"|\s[x,y]="([0-9\.]*?)"/;
-const dx = -56, dy = -32;
-let content = fs.readFileSync('point-lib-origin.svg', 'utf-8');
+
+let content = fs.readFileSync(config.fromFile, 'utf-8');
 //去掉换行和空格
 content = content.replace(/[\r\n]/g, " ");
 content.replace(/\s+/g, " ");
@@ -22,10 +30,10 @@ while (true) {
     let length = ret[0].length;
     let beforeValue = parseFloat(ret[1] || ret[2] || ret[3]);
     let isX = ret[0].indexOf('x') !== -1;
-    let afterValue = beforeValue + (isX ? dx : dy);
+    let afterValue = beforeValue + (isX ? config.dx : config.dy);
     let tmp = ret[0].replace(beforeValue, afterValue);
 
     newContent += content.substring(0, index) + tmp;
     content = content.substring(index + length, content.length);
 }
-fs.writeFileSync('ret.svg', newContent, 'utf-8');
+fs.writeFileSync(config.toFile, newContent, 'utf-8');
